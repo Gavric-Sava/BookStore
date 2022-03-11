bf7e4d8839d798a230b156b82de38819ebdb9682
Sava Gavrić <sava.gavric@logeecom.com>
2022-03-11 15:20:09 +0100

    Add Author and Book models.
    Issue MIDU-142

---
+++ src/models/Author.php
@@ -0,0 +1,60 @@
+<?php
+class Author {
+    private static $s_id = 0;
+    private int $id;
+    private string $firstname;
+    private string $lastname;
+    private array $books;
+
+    function __construct(string $firstname, string $lastname, array $books) {
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
+        $this->books = $books;
+        $this->id = Author::$s_id++;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getFirstname(): string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): void
+    {
+        $this->firstname = $firstname;
+    }
+
+    public function getLastname(): string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): void
+    {
+        $this->lastname = $lastname;
+    }
+
+    public function getBooks(): array
+    {
+        return $this->books;
+    }
+
+    public function setBooks(array $books): void
+    {
+        $this->books = $books;
+    }
+
+    public function getBookCount(): int {
+        return count($this->books);
+    }
+
+}
---
+++ src/models/Book.php
@@ -0,0 +1,45 @@
+<?php
+class Book {
+    private static $s_id = 0;
+    private int $id;
+    private string $name;
+    private int $year;
+
+    function __construct(string $name, int $year) {
+        $this->name = $name;
+        $this->year = $year;
+        $this->id = Book::$s_id++;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getYear(): int
+    {
+        return $this->year;
+    }
+
+    public function setYear(int $year): void
+    {
+        $this->year = $year;
+    }
+
+}
+
