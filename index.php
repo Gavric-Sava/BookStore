ed137e6b865d06ad51d429b35d0d7b16d9c2a6a2
Sava Gavrić <sava.gavric@logeecom.com>
2022-03-21 09:25:46 +0100

    Clean up code.
    Issue MIDU-163

--- index.php
+++ index.php
@@ -29,13 +29,11 @@ $request_path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 if (preg_match('/^\/(?:authors(?:\/.*)*)?$/', $request_path)) {
     (new AuthorController(
         new AuthorRepositoryDatabase($PDO)
-    )
-    )->process($request_path);
+    ))->process($request_path);
 } elseif (preg_match('/^\/books(?:\/.*)*$/', $request_path)) {
     (new BookController(
         new BookRepositoryDatabase($PDO)
-    )
-    )->process($request_path);
+    ))->process($request_path);
 } else {
     RequestUtil::render404();
 }
